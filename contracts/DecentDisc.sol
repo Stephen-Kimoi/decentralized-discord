61773a8a53e5a1c61fd439a427d54d470a77cd3b
Steve Kimoi <moenisteve@gmail.com>
2023-03-16 00:34:19 +0300

    createChannel and getChannel functions

--- contracts/DecentDisc.sol
+++ contracts/DecentDisc.sol
@@ -4,10 +4,31 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract DecentDisc is ERC721 {
-    address public owner; 
+    address public owner;
+    uint256 public channelNo; 
+
+    struct Channel {
+        uint256 id; 
+        string name; 
+        uint256 cost; 
+    } 
+
+    mapping (uint256 => Channel) public channels; 
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) {
         // ERC721(_name, _symbol); 
         owner = msg.sender; 
     } 
+
+    
+
+    function createChannel(string memory name, uint256 cost) public {
+        require(msg.sender == owner, "ERC721: Not owner of channel!"); 
+        channelNo += 1; 
+        channels[channelNo] = Channel(channelNo, name, cost); 
+    }
+
+    function getChannel(uint256 id) public view returns (Channel memory){
+        return channels[id]; 
+    }
 }
