c9b98af93c25399a46b86e2fdafb768ebded8518
Steve Kimoi <moenisteve@gmail.com>
2023-03-16 00:35:07 +0300

    testing createChannel and getChannel functionalities

--- test/DecentDisc.js
+++ test/DecentDisc.js
@@ -19,6 +19,9 @@ describe("DecentDisc", () => {
 
         const contractFactory = await ethers.getContractFactory("DecentDisc"); 
         decentContract = await contractFactory.deploy(NAME, SYMBOL); 
+
+        const tx = await decentContract.connect(owner).createChannel("Web3 Devs", tokens(1)); 
+        await tx.wait(); 
    })
 
    describe("deployment", () => {
@@ -37,6 +40,22 @@ describe("DecentDisc", () => {
         let owner = await decentContract.owner(); 
         expect(owner).to.equal(OWNER.address); 
       })
+
+   })
+
+   describe("Creating channels", () => {
+
+       it("Returns total channels", async () => {
+        const totalChannels = await decentContract.channelNo()
+        expect(totalChannels).to.equal(1); 
+       })
+
+       it('Returns channel details', async () => {
+        const channel = await decentContract.getChannel(1); 
+        expect(channel.id).to.equal(1); 
+        expect(channel.name).to.equal("Web3 Devs"); 
+        expect(channel.cost).to.equal(tokens(1)); 
+       })
 
    })
 })
