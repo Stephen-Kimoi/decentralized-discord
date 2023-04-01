8d57100e542c303a18a4bcf79e5ee7f858dd907a
Steve Kimoi <moenisteve@gmail.com>
2023-04-01 23:13:24 +0300

    code for deploying contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,32 +1,22 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+const hre = require("hardhat"); 
 
-async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether")
+} 
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+const main = async () => {
+  const name = "DecDisc"; 
+  const symbol = "DDSC"; 
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  const [deployer] = await ethers.getSigners(); 
+  const contractFactory = await ethers.getContractFactory("DecentDisc"); 
+  const decentDisc = await contractFactory.deploy(); 
+  await decentDisc.deployed();  
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  console.log('Contract address: ', decentDisc.address); 
+  
+  for(let i = 0; i < 3; i++){ 
+    
+  }
 }
-
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
