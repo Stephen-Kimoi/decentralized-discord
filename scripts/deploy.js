d1f81d621b80be0f046046b329675a9ae3cb85e9
Steve Kimoi <moenisteve@gmail.com>
2023-04-01 23:31:30 +0300

    adding the createChannel functions

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,8 +15,14 @@ const main = async () => {
   await decentDisc.deployed();  
 
   console.log('Contract address: ', decentDisc.address); 
+
+  const channel_names = ["Intro", "General", "Showcase"]; 
+  const cost = [tokens(0), tokens(1), tokens(2.5)]
   
   for(let i = 0; i < 3; i++){ 
-    
+    const tx = await decentDisc.connect(deployer).createChannel(channel_names[i], cost[i])
+    await tx.wait(); 
+
+    console.log("Created channel: ", channel_names[i]); 
   }
 }
