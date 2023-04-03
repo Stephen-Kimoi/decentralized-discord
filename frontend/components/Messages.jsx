7c80e6885942bfb11638c369c95e5e21e9da1ccc
Steve Kimoi <moenisteve@gmail.com>
2023-04-03 09:41:22 +0300

    messages starrter code

---
+++ frontend/components/Messages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Messages.css'
+
+const Messages = () => {
+  return (
+    <div className='messages'>
+      Messages
+    </div>
+  )
+}
+
+export default Messages
