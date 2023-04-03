a621c3eea66fcf06e8453c123ae2c42b1bb67b65
Steve Kimoi <moenisteve@gmail.com>
2023-04-03 09:42:03 +0300

    server starter code

---
+++ frontend/components/Server.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './styles/Server.css'
+
+const Server = () => {
+  return (
+    <div className="servers">
+
+        <div className="server">
+          <img src='./ethereum.svg' alt="Ethereum Logo" />
+        </div>
+
+        <div className="server">
+          <img src='./plus.svg' alt="Add Server" />
+        </div>
+
+        <div className="server">
+          <img src='./search.svg' alt="Search" />
+        </div>
+
+  </div>
+  )
+}
+
+export default Server
