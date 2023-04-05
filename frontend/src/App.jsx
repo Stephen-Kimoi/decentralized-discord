50a3956d2b62cdac1a8b3216460f940c2a80fad4
Steve Kimoi <moenisteve@gmail.com>
2023-04-05 09:55:00 +0300

    connecting blockchain to frontend and getting the channels created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,17 +4,42 @@ import Navbar from '../components/Navbar'
 import Server from '../components/Server';
 import Channel from '../components/Channel';
 import Messages from '../components/Messages';
+import abi from '../abi/DecentDisc.json'; 
+import config from '../config.json';
+
 
 function App() {
   const [isDarkMode, setIsDarkMode] = useState(false); 
   const [account, setAccount] = useState(); 
   const {walletConnected, setWalletConnected} = useState(false); 
+  const [provider, setProvider] = useState(); 
+  const [decentDisc, setDecentDisc] = useState(); 
+  const [channels, setChannels] = useState([]); 
 
   const handleToggleDarkMode = () => {
     setIsDarkMode(!isDarkMode); 
   }
 
   const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum); 
+    setProvider(provider); 
+    
+    const network = await provider.getNetwork(); 
+    
+    const decentDisc = new ethers.Contract(config[network.chainId].DecentDisc.address, abi.abi, provider); 
+    setDecentDisc(decentDisc); 
+
+    const allChannels = await decentDisc.channelNo(); 
+    const channels = []; 
+    // console.log("Total channels: ", allChannels.toString()); 
+
+    for (let i = 0; i <= allChannels; i++) {
+      const channel = await decentDisc.getChannel(i); 
+      channels.push(channel); 
+    }
+
+    setChannels(channels)
+
     window.ethereum.on('accountsChanged', function () {
       window.location.reload()
     })
@@ -42,6 +67,10 @@ function App() {
 
         <Channel 
           handleToggleDarkMode={handleToggleDarkMode}
+          provider={provider}
+          account={account}
+          decentDisc={decentDisc}
+          channels={channels}
         /> 
 
         <Messages 
