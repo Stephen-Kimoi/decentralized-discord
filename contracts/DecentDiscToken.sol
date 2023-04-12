85a79c812dc85ffd4c1a9f436b5625bd0df16e27
Steve Kimoi <moenisteve@gmail.com>
2023-04-12 22:23:56 +0300

    updated decentDisc token contract

--- contracts/DecentDiscToken.sol
+++ contracts/DecentDiscToken.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol"; 
+import "hardhat/console.sol";
 
 contract DecentDiscToken is ERC20 {
-    constructor(address decentDisc) ERC20("Decent Disc Token", "DDT"){
+    address public contractAddress; 
+
+   constructor(address decentDisc, uint256 amount) ERC20("Decent Disc Token", "DDT"){
         _mint(decentDisc, 10000 * 10 ** 18);
+        _transfer(decentDisc, address(this), amount);
     }
 }
