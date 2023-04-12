5992aff6cc2373b03e445f0971bee19aa2f99882
Steve Kimoi <moenisteve@gmail.com>
2023-04-12 22:25:51 +0300

    new deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,8 @@
 // Contract address:  0x5FbDB2315678afecb367f032d93F642f64180aa3
+
+// Decent Disc contract address:  0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9
+// Token contract address:  0x5FC8d32690cc91D4c39d9d3abcBD16989F875707
+
 const { ethers } = require("hardhat");
 const hre = require("hardhat"); 
 
@@ -12,10 +16,15 @@ const main = async () => {
 
   const [deployer] = await ethers.getSigners(); 
   const contractFactory = await ethers.getContractFactory("DecentDisc"); 
+  const contractFactory2 = await ethers.getContractFactory("DecentDiscToken"); 
   const decentDisc = await contractFactory.deploy(name, symbol); 
   await decentDisc.deployed();  
 
-  console.log('Contract address: ', decentDisc.address); 
+  const tokenContract = await contractFactory2.deploy(decentDisc.address, tokens(10)); 
+  await tokenContract.deployed();  
+
+  console.log('Decent Disc contract address: ', decentDisc.address); 
+  console.log('Token contract address: ', tokenContract.address); 
 
   const channel_names = ["Intro", "General", "Showcase"]; 
   const cost = [tokens(0), tokens(1), tokens(2.5)]
