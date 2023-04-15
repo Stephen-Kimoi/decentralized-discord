e9dbdfef64b8a4837eca217bfb30adc4029ec986
Steve Kimoi <moenisteve@gmail.com>
2023-04-15 23:28:49 +0300

    automatically siging the transaction without wallet prompt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,7 +13,7 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:3030'); 
 
 // const PRIVATE_KEY = process.env.LOCALNETWORK_PRIVATE_KEY; 
-const PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; 
+const PRIVATE_KEY = "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80"; // Hardhat node wallet address **DO NOT SEND FUNDS!
 const rpcProvider = "http://localhost:8545"; 
 
 
