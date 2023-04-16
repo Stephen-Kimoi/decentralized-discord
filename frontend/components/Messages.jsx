9732be0c1e01cc4de7422ee26badc3e5488bd262
Steve Kimoi <moenisteve@gmail.com>
2023-04-16 12:40:39 +0300

    adding points and checking points for accounts

--- frontend/components/Messages.jsx
+++ frontend/components/Messages.jsx
@@ -5,13 +5,80 @@ import './styles/Messages.css'
 
 const socket = io('http://localhost:3030'); 
 
-const Messages = ({account, messages, currentChannel}) => {
+const Messages = ({account, messages, currentChannel, accountsWithMorePoints, accountsSentMessages, newAccountPoints, setNewAccountPoints, channelCreators, setChannelCreators}) => {
   const [message, setMessage] = useState(''); 
-
+  // const [s]
   const messageEndRef = useRef(); 
+   
+  const addPoint = () => { 
+    // console.log(123343); 
+    let updatedAccountPoints
+    const index = newAccountPoints.findIndex((item) => item.account === account)
+    // console.log(`Index of ${account} is: `, index); 
+    if (index !== -1){
+      // console.log(`Points for ${account} is: `, newAccountPoints[index].points);
+      const updatedAccount = {
+        ...newAccountPoints[index], 
+        points: newAccountPoints[index].points + 1
+      }; 
+      // console.log(`Points for ${account} is ${updatedAccount.points}`)
+      // console.log('Account points: ', [...newAccountPoints])
+      updatedAccountPoints = [...newAccountPoints]; 
+      updatedAccountPoints[index] = updatedAccount; 
+      // console.log("UPDATED ACCOUNTS POINTS: ", updatedAccountPoints); 
+      setNewAccountPoints(updatedAccountPoints); 
+    }
+
+    const checkAccountPoints = () => {
+      console.log("UPDATED ACCOUNTS: ", updatedAccountPoints)
+      const index = updatedAccountPoints.findIndex((item) => item.account === account); 
+      let result;      
+
+      if(index !== -1){
+        const points = newAccountPoints[index].points; 
+        // console.log(`Points for ${account} are ${points}`); 
+        if (points > 3){
+          // console.log("TRUE!")
+          result = true 
+        } else {
+          // console.log("FALSE!")
+          result = false 
+        }
+      }
+
+      return result; 
+    }
+
+    const result = checkAccountPoints();
+    return result;
+  }
+
+  const addChannelCreator = (account) => {
+    let updatedChannelCreators; 
+    if(!channelCreators.find((creator) => creator === account)){
+      // console.log(`${account} is not amoung channel creators`)
+      updatedChannelCreators = [...channelCreators, account]
+      // console.log("Updated channel creators: ", updatedChannelCreators); 
+      setChannelCreators(updatedChannelCreators)
+
+      const channelCreator = {
+        account: account
+      }; 
+
+      socket.emit('new channel creator', channelCreator); 
+
+    } else {
+      // console.log(`${account} is among the channel creators`)
+      updatedChannelCreators = channelCreators; 
+    }
+    
+    // console.log("Updated channel creators: ", updatedChannelCreators); 
+    return updatedChannelCreators; 
+  }
 
   const sendMessage = async (e) => {
     e.preventDefault(); 
+    // const updatedChannelCreators; 
 
     const messageObj = {
       channel: currentChannel.id.toString(), 
@@ -20,7 +87,16 @@ const Messages = ({account, messages, currentChannel}) => {
     }
 
     if (message !== ""){
-      socket.emit('new message', messageObj)
+      socket.emit('new message', messageObj);  
+      const result = addPoint();
+      // console.log(`Result for ${account} is ${result}`)
+      
+      // console.log("Channel creators: ", channelCreators); 
+      if (result) {
+        const updatedChannelCreators = addChannelCreator(account); 
+        console.log('UPDATED CHANNEL CREATORS: ', updatedChannelCreators);
+      } 
+
     }
     setMessage(""); 
 
@@ -33,8 +109,10 @@ const Messages = ({account, messages, currentChannel}) => {
     }, 500)
   }
 
+
   useEffect(() => {
-    scrollHandler()
+    scrollHandler(); 
+    // checkAccountPoints(); 
   }, [])
 
   return (
