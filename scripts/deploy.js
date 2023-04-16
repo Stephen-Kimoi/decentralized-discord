e59112da5cc641cec89a88c20017351a16873e02
Steve Kimoi <moenisteve@gmail.com>
2023-04-16 12:39:15 +0300

    new contract address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -74,3 +74,4 @@ runMain();
 // Decent Disc contract address:  0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512
 // Token contract address:  0x5FbDB2315678afecb367f032d93F642f64180aa3
 // Deployer address:  0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
